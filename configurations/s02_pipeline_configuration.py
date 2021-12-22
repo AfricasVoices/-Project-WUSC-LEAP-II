5619b652c6935feca37c66cfdbd2c85a75fe4629
IsaackMwenda <mwendaisaac@gmail.com>
2021-12-22 08:14:27 +0300

    add code scheme count ep 5

--- configurations/s02_pipeline_configuration.py
+++ configurations/s02_pipeline_configuration.py
@@ -139,7 +139,7 @@ PIPELINE_CONFIGURATION = PipelineConfiguration(
                     coda_dataset_id="LEAP_s02e05",
                     engagement_db_dataset="leap_s02e05",
                     code_scheme_configurations=[
-                        CodeSchemeConfiguration(code_scheme=load_code_scheme("s02e05"), auto_coder=None)
+                        CodeSchemeConfiguration(code_scheme=load_code_scheme("s02e05"), auto_coder=None, coda_code_schemes_count=3)
                     ],
                     ws_code_string_value="leap_s02e05"
                 ),
